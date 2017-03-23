1211df31ae99b426db2bcaca7958699eec9fef5e
akahard2dj <akahard2dj@naver.com>
2017-03-23 15:48:01 +0900

    Update views.py

--- board/views.py
+++ board/views.py
@@ -83,10 +83,57 @@ class PostList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     '''
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    permissions_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
-    serializer_class = PostDetailSerializer
+
+class PostAdd(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def post(self, request, format=None):
+        item = request.data
+        item.update({'owner': request.user.id})
+
+        serializer = PostDetailSerializer(data=item)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response({'msg': 'failed'}, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        if request.user.id != post.owner_id:
+            view_count = post.views
+            post.views = view_count + 1
+            post.save()
+        serializer = PostDetailSerializer(post)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        item = request.data
+        item.update({'owner': request.user.id})
+
+        serializer = PostDetailSerializer(data=item)
+        if serializer.is_valid():
+            post.title = item['title']
+            post.content = item['content']
+            post.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response({'msg': 'success'}, status=status.HTTP_202_ACCEPTED)
 
 
 class UserDetail(APIView):
