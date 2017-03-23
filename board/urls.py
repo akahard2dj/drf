59501ea1afc5d6a34274a907b3533dd7d20f6c6f
akahard2dj <akahard2dj@naver.com>
2017-03-23 13:54:47 +0900

    Update urls.py

--- board/urls.py
+++ board/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     url(r'^posts/$', views.PostList.as_view()),
     url(r'^posts/(?P<pk>[0-9]+)/$', views.PostDetail.as_view()),
 
-    url(r'users/$', views.UserList.as_view()),
-    url(r'users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'users/$', views.UserDetail.as_view()),
+    #url(r'users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'registration/$', views.registration),
     url(r'registration/confirmation/$', views.registration_confirmation),
 
