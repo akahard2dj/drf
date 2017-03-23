cd931276c1692fb78d56748115d2b41a6c5c7691
akahard2dj <akahard2dj@naver.com>
2017-03-23 11:30:24 +0900

    Create models.py

---
+++ board/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    owner = models.ForeignKey('core.MyUser', related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created_at"]
