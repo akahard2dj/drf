8c5a427c67903b1858ab6bdc4cfca9f97e1e5df8
akahard2dj <akahard2dj@naver.com>
2017-03-30 10:40:01 +0900

    user model is changed (added nickname)

--- core/models.py
+++ core/models.py
@@ -34,6 +34,7 @@ class MyUser(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    nickname = models.CharField(null=False, max_length=100, default=random_digit_and_number)
     #date_of_birth = models.DateField()
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
