e128589652872981d37fd7c0abe7195351316f24
akahard2dj <akahard2dj@naver.com>
2017-04-02 21:43:02 +0900

    logic changing..

--- board/views.py
+++ board/views.py
@@ -19,6 +19,7 @@ from core.models.category import (University, Department)
 from core.models.name_tag import NameTag
 from core.celery_email import Mailer
 from core.utils import random_digit_and_number
+from core.utils import is_valid_email
 
 from celery import shared_task
 
@@ -37,55 +38,60 @@ def celery_test(request):
 
 @api_view(['POST'])
 def pre_check(request):
-    data = request.data
+    request_data = request.data
 
-    try:
-        token = data['token']
-    except MultiValueDictKeyError:
+    is_token_key = 'token' in request_data
+    if is_token_key:
+        token = request_data['token']
+
+        if Token.objects.filter(key=token).exists():
+            return Response({'msg': 'success'}, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response({'msg': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
         return Response({'msg': 'No Token'}, status=status.HTTP_401_UNAUTHORIZED)
 
-    try:
-        _ = Token.objects.get(key=token)
-    except Token.DoesNotExist:
-        return Response({'msg': 'Invalid Token'}, status=status.HTTP_401_UNAUTHORIZED)
-    
-    return Response({'msg': 'success'}, status=status.HTTP_202_ACCEPTED)
 
-
-@api_view(['POST'])
+@never_cache
+@api_view(['GET'])
 def gen_auth_key(request):
     request_data = request.data
-    try:
-        key = request_data['email']
-    except MultiValueDictKeyError:
-        return Response({'msg': 'No Email'}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    auth_code = random_digit_and_number(length_of_value=6)
-    print(auth_code)
-    if cache.get(key):
-        return Response({'msg': 'processing Authorization, check the own email'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+    is_email_key = 'email' in request_data
+    if is_email_key:
+        key_email = request_data['email']
+
+        if is_valid_email(key_email):
+
+            if cache.get(key_email):
+                return Response({'msg': 'processing Authorization, check the own email'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+            else:
+                auth_code = random_digit_and_number(length_of_value=6)
+                print(auth_code)
+                cache_data = {'code': auth_code, 'status': 'SENT'}
+                cache.set(key_email, cache_data, timeout=300)
+                #m = Mailer()
+                #m.send_messages('Authorization Code', temp_value, [key])
+
+                return Response({'msg': 'success'}, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response({'msg': 'Invalid Email'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        cache_data = {'code': auth_code, 'status': 'SENT'}
-        cache.set(key, cache_data, timeout=300)
-        #m = Mailer()
-        #m.send_messages('Authorization Code', temp_value, [key])
-
-        return Response({'msg': 'success'}, status=status.HTTP_202_ACCEPTED)
+        return Response({'msg': 'No Email'}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
 @api_view(['POST'])
 def confirm_auth_key(request):
     request_data = request.data
 
-    try:
-        key_email = request_data['email']
-    except MultiValueDictKeyError:
-        return Response({'msg': 'No Email'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    is_email_key = 'email' in request_data
+    is_authcode_key = 'auth_code' in request_data
 
-    try:
+    if is_email_key and is_authcode_key:
+        key_email = request_data['email']
         auth_code = request_data['auth_code']
-    except MultiValueDictKeyError:
-        return Response({'msg': 'No Auth_code'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    else:
+        return Response({'msg': 'Not enough data'}, status=status.HTTP_400_BAD_REQUEST)
 
     value_from_cache = cache.get(key_email)
 
@@ -94,16 +100,13 @@ def confirm_auth_key(request):
 
     else:
         if auth_code == value_from_cache['code']:
-            try:
-                user_from_db = DonkeyUser.objects.get(email=key_email)
-            except DonkeyUser.DoesNotExist:
+            if DonkeyUser.objects.filter(email=key_email).exists():
+                cache.delete(key_email)
+                return Response({'msg': 'existed user'}, status=status.HTTP_202_ACCEPTED)
+            else:
                 value_from_cache['status'] = 'CONFIRM'
                 cache.set(key_email, value_from_cache, timeout=600)
                 return Response({'msg': 'new user'}, status=status.HTTP_202_ACCEPTED)
-            else:
-                cache.delete(key_email)
-                return Response({'msg': 'existed user'}, status=status.HTTP_202_ACCEPTED)
-
         else:
             return Response({'msg': 'failed'}, status=status.HTTP_401_UNAUTHORIZED)
 
@@ -112,15 +115,14 @@ def confirm_auth_key(request):
 def registration(request):
     request_data = request.data
 
-    try:
-        key_email = request_data['email']
-    except MultiValueDictKeyError:
-        return Response({'msg': 'No Email'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    is_email_key = 'email' in request_data
+    is_authcode_key = 'auth_code' in request_data
 
-    try:
+    if is_email_key and is_authcode_key:
+        key_email = request_data['email']
         auth_code = request_data['auth_code']
-    except MultiValueDictKeyError:
-        return Response({'msg': 'No Auth_code'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+    else:
+        return Response({'msg': 'Not enough data'}, status=status.HTTP_400_BAD_REQUEST)
 
     value_from_cache = cache.get(key_email)
 
