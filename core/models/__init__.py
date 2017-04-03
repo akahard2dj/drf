63f29aa2b290a792fea5fcb009c4805ae603e6e0
akahard2dj <akahard2dj@naver.com>
2017-04-03 16:02:44 +0900

    Update __init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,5 @@
 from .donkey_user import DonkeyUser
 from .category import (University, Department)
 from .name_tag import NameTag
+from .connector import Connector
+from .bulletin_board import BulletinBoard
