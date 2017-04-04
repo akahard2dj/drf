271a419760315376cedacc9b04a18ed86c91df66
akahard2dj <akahard2dj@naver.com>
2017-04-04 14:46:00 +0900

    Update urls.py

--- board/urls.py
+++ board/urls.py
@@ -12,9 +12,14 @@ urlpatterns = [
     url(r'^confirm-auth-key/$', views.confirm_auth_key, name='confirm-auth-key'),
 
     # registration : user registration
-    url(r'^registration/$', views.registration),
+    url(r'^registration/$', views.registration, name='registration'),
 
-    url('^hello/', views.hello)
+    # articles : listing articles
+    # articles-add : adding an article
+    url(r'^articles/$', views.ArticleList.as_view(), name='articles'),
+    url(r'^article/add/$', views.ArticleAdd.as_view(), name='articles-add'),
+    url(r'^articles/(?P<pk>[0-9]+)/$', views.ArticleDetail.as_view(), name='articles-detail')
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
