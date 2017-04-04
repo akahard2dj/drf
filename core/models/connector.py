7528b34f0d2f26965492067f39d01b387e884465
akahard2dj <akahard2dj@naver.com>
2017-04-04 14:47:25 +0900

    Update connector.py

--- core/models/connector.py
+++ core/models/connector.py
@@ -2,7 +2,7 @@ from django.db import models
 from core.models.donkey_user import DonkeyUser
 
 
-class Connector(models.Model):
+class UserBoardConnector(models.Model):
     donkey_user = models.OneToOneField(DonkeyUser)
     bulletin_board_id_str = models.TextField(null=True)
 
@@ -13,5 +13,32 @@ class Connector(models.Model):
         else:
             value += format('{},'.format(bulletinboard_id))
 
-        self.bulletin_board_id_str = value
+        id_list = list(map(int, value.split(',')[:-1]))
+        id_list = sorted(set(id_list))
+        value_to_db = ','.join(str(x) for x in id_list) + ','
+        self.bulletin_board_id_str = value_to_db
         self.save()
+
+    def get_bulletinboard_id(self):
+        value = self.bulletin_board_id_str
+        return list(map(int, value.split(',')[:-1]))
+
+    def pop_bulletinboard_id(self, bulletinboard_id):
+        value = self.bulletin_board_id_str
+        id_list = list(map(int, value.split(',')[:-1]))
+        if bulletinboard_id in id_list:
+            id_list.remove(bulletinboard_id)
+            value_to_db = ','.join(str(x) for x in id_list) + ','
+            self.bulletin_board_id_str = value_to_db
+            self.save()
+
+    def check_bulletinboard_id(self, bulletinboard_id):
+        bulletinboard_id = int(bulletinboard_id)
+
+        value = self.bulletin_board_id_str
+        if not value:
+            return False
+        else:
+            id_list = list(map(int, value.split(',')[:-1]))
+
+            return bulletinboard_id in id_list
