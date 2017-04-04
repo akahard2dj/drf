27a6fbff8a7c37408670881c05522ae5e02e36bc
akahard2dj <akahard2dj@naver.com>
2017-04-04 14:45:19 +0900

    Update permissions.py

--- board/permissions.py
+++ board/permissions.py
@@ -7,3 +7,23 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         #    return True
         print(obj.owner, request.user)
         return request.user == obj
+
+
+class IsDeleteAndIsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        is_owner = request.user == obj
+        is_delete = request.method == 'DELETE'
+
+        return is_owner and is_delete
+
+
+class IsPostAndIsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            is_post = True
+        else:
+            is_post = False
+
+        is_auth = request.user and request.user.is_authenticated()
+
+        return is_post and is_auth
