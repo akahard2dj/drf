0b1359da3cd0dcbf7e228daf09b258fdd6421ac7
akahard2dj <akahard2dj@naver.com>
2017-04-11 16:56:56 +0900

    Update authentication.py

--- core/authentication.py
+++ core/authentication.py
@@ -1,17 +1,66 @@
-from rest_framework import authentication
-from rest_framework import exceptions
-from rest_framework.authtoken.models import Token
+from rest_framework.authentication import BaseAuthentication
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
+
+from django.utils.six import text_type
+from django.utils.translation import ugettext_lazy as _
+from django.core.cache import cache
 
 
-class BoraApiAuthentication(authentication.BaseAuthentication):
+def get_bora_authorization_header(request):
+    auth = request.META.get('HTTP_X_AUTH_TOKEN', b'')
+    if isinstance(auth, text_type):
+        # Work around django test client oddness
+        auth = auth.encode(HTTP_HEADER_ENCODING)
+    return auth
+
+
+class BoraApiAuthentication(BaseAuthentication):
+    keyword = 'BoraToken'
+    model = None
+
+    def get_model(self):
+        if self.model is not None:
+            return self.model
+        from rest_framework.authtoken.models import Token
+        return Token
+
+    def authenticate_header(self, request):
+        return self.keyword
+
     def authenticate(self, request):
-        token = request.META.get('HTTP_X_TOKEN')
-        if not token:
+        auth = get_bora_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
             return None
 
-        try:
-            user = Token.objects.get(key=token)
-        except Token.DoesNotExist:
-            raise exceptions.AuthenticationFailed('No such user')
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
 
-        return user, True
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
+
+    def authenticate_credentials(self, key):
+        model = self.get_model()
+
+        if cache.get(key):
+            value_from_cache = cache.get(key)
+            return value_from_cache['user'], key
+        else:
+            try:
+                token = model.objects.get(key=key)
+            except model.DoesNotExist:
+                raise exceptions.AuthenticationFailed(_('Invalid token'))
+            else:
+                cache_data = {'user': token.user}
+                cache.set(token.key, cache_data, 86400)
+
+        return token.user, token
